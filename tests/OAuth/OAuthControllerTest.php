73a0c3f637aa2f08aa7712081cab416bae98f523
Luc Wollants <luc.wollants@apptite.be>
2016-04-28 09:21:41 +0200

    III-889 Added stub for unit test of OAuthController.

---
+++ tests/OAuth/OAuthControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CultuurNet\UDB3\JwtProvider\OAuth;
+
+class OAuthControllerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function testStub()
+    {
+
+    }
+}
