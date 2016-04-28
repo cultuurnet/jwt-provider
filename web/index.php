fd60c4dab0b40ed8d2b5496c57a0a117b071cc3f
Luc Wollants <luc.wollants@apptite.be>
2016-04-28 15:49:56 +0200

    III-889 Register all service and controller providers.

--- web/index.php
+++ web/index.php
@@ -2,8 +2,14 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use CultuurNet\UDB3\JwtProvider\OAuth\OAuthControllerProvider;
+use CultuurNet\UDB3\JwtProvider\OAuth\OAuthServiceProvider;
+use CultuurNet\UDB3\JwtProvider\OAuth\OAuthUrlHelperServiceProvider;
+use CultuurNet\UDB3\JwtProvider\RequestTokenStorage\RequestTokenStorageServiceProvider;
 use Silex\Application;
 use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\SessionServiceProvider;
+use Silex\Provider\UrlGeneratorServiceProvider;
 
 /** @var Application $app */
 $app = require __DIR__ . '/../bootstrap.php';
@@ -12,5 +18,13 @@ $app = require __DIR__ . '/../bootstrap.php';
  * Allow to use services as controllers.
  */
 $app->register(new ServiceControllerServiceProvider());
+$app->register(new SessionServiceProvider());
+$app->register(new UrlGeneratorServiceProvider());
+
+$app->register(new RequestTokenStorageServiceProvider());
+$app->register(new OAuthUrlHelperServiceProvider());
+$app->register(new OAuthServiceProvider());
+
+$app->mount('culturefeed/oauth', new OAuthControllerProvider());
 
 $app->run();
