2ca2276925de8856f0f7623ac96e2f1d8ada37ff
Luc Wollants <luc.wollants@apptite.be>
2016-05-03 21:38:10 +0200

    III-890 Setup service providers (still issue with access token for the culture feed client).

--- web/index.php
+++ web/index.php
@@ -2,9 +2,11 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use CultuurNet\UDB3\JwtProvider\Jwt\JwtOAuthCallbackHandlerServiceProvider;
 use CultuurNet\UDB3\JwtProvider\OAuth\OAuthControllerProvider;
 use CultuurNet\UDB3\JwtProvider\OAuth\OAuthServiceProvider;
 use CultuurNet\UDB3\JwtProvider\OAuth\OAuthUrlHelperServiceProvider;
+use CultuurNet\UDB3\JwtProvider\User\CultureFeedUserServiceProvider;
 use CultuurNet\UDB3\JwtProvider\RequestTokenStorage\RequestTokenStorageServiceProvider;
 use Silex\Application;
 use Silex\Provider\ServiceControllerServiceProvider;
@@ -21,6 +23,8 @@ $app->register(new ServiceControllerServiceProvider());
 $app->register(new SessionServiceProvider());
 $app->register(new UrlGeneratorServiceProvider());
 
+$app->register(new JwtOAuthCallbackHandlerServiceProvider());
+$app->register(new CultureFeedUserServiceProvider());
 $app->register(new RequestTokenStorageServiceProvider());
 $app->register(new OAuthUrlHelperServiceProvider());
 $app->register(new OAuthServiceProvider());
