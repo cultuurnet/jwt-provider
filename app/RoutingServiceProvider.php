dfc85d0db5d01397d7643c71b306f66b4046292a
Bert Ramakers <bert@madewithlove.be>
2019-12-24 15:03:14 +0100

    Register HTTP routes

--- app/RoutingServiceProvider.php
+++ app/RoutingServiceProvider.php
@@ -2,11 +2,14 @@
 
 namespace CultuurNet\UDB3\JwtProvider;
 
+use CultuurNet\UDB3\JwtProvider\OAuth\OAuthController;
 use League\Route\Router;
 use League\Route\Strategy\ApplicationStrategy;
 
 class RoutingServiceProvider extends BaseServiceProvider
 {
+    public const AUTHORIZATION_PATH = '/authorize';
+
     protected $provides = [
         Router::class,
     ];
@@ -19,6 +22,17 @@ class RoutingServiceProvider extends BaseServiceProvider
                 $router = new Router();
                 $strategy = (new ApplicationStrategy())->setContainer($this->getContainer());
                 $router->setStrategy($strategy);
+
+                $router->get('/connect', [OAuthController::class, 'connect']);
+                $router->get('/register', [OAuthController::class, 'register']);
+                $router->get('/logout', [OAuthController::class, 'logout']);
+                $router->get(self::AUTHORIZATION_PATH, [OAuthController::class, 'authorize']);
+
+                // Maintain these old paths for backwards compatibility.
+                $router->get('/culturefeed/oauth/connect', [OAuthController::class, 'connect']);
+                $router->get('/culturefeed/oauth/authorize', [OAuthController::class, 'authorize']);
+                $router->get('/culturefeed/oauth/register', [OAuthController::class, 'register']);
+                $router->get('/culturefeed/oauth/logout', [OAuthController::class, 'logout']);
 
                 return $router;
             }
