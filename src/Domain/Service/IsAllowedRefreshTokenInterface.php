4109bbcd7bea8f76e444cd4460f48755c79a1eec
zvonimir <zspajich@gmail.com>
2020-01-28 21:38:05 +0100

    Implement IsAllowedRefreshToken service

---
+++ src/Domain/Service/IsAllowedRefreshTokenInterface.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace CultuurNet\UDB3\JwtProvider\Domain\Service;
+
+use ValueObjects\StringLiteral\StringLiteral;
+
+interface IsAllowedRefreshTokenInterface
+{
+    public function __invoke(StringLiteral $apiKey) : bool;
+}
