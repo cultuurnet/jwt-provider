ea836450e52db91ad0b59b8bdc771fa927c7fa4b
zvonimir <zspajich@gmail.com>
2020-01-28 21:39:26 +0100

    Add ClientInformation value object

---
+++ src/Domain/Value/ClientInformation.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace CultuurNet\UDB3\JwtProvider\Domain\Value;
+
+use Psr\Http\Message\UriInterface;
+use ValueObjects\StringLiteral\StringLiteral;
+
+class ClientInformation
+{
+    /**
+     * @var UriInterface
+     */
+    private $uri;
+
+    /**
+     * @var StringLiteral
+     */
+    private $apiKey;
+    
+    /**
+     * @var bool
+     */
+    private $isAllowedRefresh;
+
+    public function __construct(UriInterface $uri, StringLiteral $apiKey = null, bool $isAllowedRefresh = false)
+    {
+        $this->uri = $uri;
+        $this->apiKey = $apiKey;
+        $this->isAllowedRefresh = $isAllowedRefresh;
+    }
+
+    public function uri(): UriInterface
+    {
+        return $this->uri;
+    }
+
+    public function apiKey(): ?StringLiteral
+    {
+        return $this->apiKey;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAllowedRefresh(): bool
+    {
+        return $this->isAllowedRefresh;
+    }
+}
