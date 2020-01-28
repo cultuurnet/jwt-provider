4a1e61593bfbe7099f65e2d288747f50c8654f77
zvonimir <zspajich@gmail.com>
2020-01-28 22:43:53 +0100

    Update LoginAuth0ServiceaAdapter

--- tests/Infrastructure/Service/LoginAuth0AdapterTest.php
+++ tests/Infrastructure/Service/LoginAuth0AdapterTest.php
@@ -2,15 +2,11 @@
 
 namespace CultuurNet\UDB3\JwtProvider\Infrastructure\Service;
 
-use Auth0\SDK\API\Authentication;
 use Auth0\SDK\Auth0;
 use Auth0\SDK\Exception\ApiException;
 use Auth0\SDK\Exception\CoreException;
 use CultuurNet\UDB3\JwtProvider\Domain\Exception\UnSuccessfulAuthException;
-use CultuurNet\UDB3\JwtProvider\Infrastructure\Factory\SlimResponseFactory;
-use Fig\Http\Message\StatusCodeInterface;
 use PHPUnit\Framework\TestCase;
-use Slim\Psr7\Factory\UriFactory;
 
 class LoginAuth0AdapterTest extends TestCase
 {
@@ -46,11 +42,29 @@ class LoginAuth0AdapterTest extends TestCase
         $this->assertEquals('token', $auth0adapter->token());
     }
 
+    /**
+     * @test
+     */
+    public function it_returns_refresh_token()
+    {
+
+        $auth0 = $this->prophesize(Auth0::class);
+
+        $auth0adapter = new LoginAuth0Adapter(
+            $auth0->reveal()
+        );
+
+        $auth0->getRefreshToken()->willReturn('refresh-token');
+
+        $this->assertEquals('refresh-token', $auth0adapter->refreshToken());
+    }
 
     /**
      * @test
      * @dataProvider auth0_exceptions()
      * @param string $exceptionClassName
+     * @throws ApiException
+     * @throws CoreException
      * @throws UnSuccessfulAuthException
      */
     public function it_wraps_auth0_exceptions_to_unsuccessful_auth_exception(string $exceptionClassName): void
