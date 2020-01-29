d9d285f4acd1afaad30833c2d2377b623dcec0c7
zvonimir <zspajich@gmail.com>
2020-01-29 07:47:47 +0100

    Register error handler

--- web/index.php
+++ web/index.php
@@ -2,19 +2,21 @@
 
 use CultuurNet\UDB3\JwtProvider\Factory\ConfigFactory;
 use CultuurNet\UDB3\JwtProvider\Factory\ContainerFactory;
+use CultuurNet\UDB3\JwtProvider\Factory\ErrorHandlerFactory;
 use League\Route\Router;
 use Slim\Psr7\Factory\ServerRequestFactory;
 use Zend\HttpHandlerRunner\Emitter\SapiStreamEmitter;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$config = ConfigFactory::create(__DIR__ . '/../');
+$whoops = ErrorHandlerFactory::create();
+$whoops->register();
 
+$config = ConfigFactory::create(__DIR__ . '/../');
 $container = ContainerFactory::forWeb($config);
 
 $response = $container->get(Router::class)->dispatch(
     ServerRequestFactory::createFromGlobals()
 );
-
 
 (new SapiStreamEmitter())->emit($response);
