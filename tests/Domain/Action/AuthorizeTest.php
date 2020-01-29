2c790b04c698c49ebb442d83f83fe665000e4c09
zvonimir <zspajich@gmail.com>
2020-01-29 07:49:51 +0100

    Fix test

--- tests/Domain/Action/AuthorizeTest.php
+++ tests/Domain/Action/AuthorizeTest.php
@@ -4,7 +4,6 @@ namespace CultuurNet\UDB3\JwtProvider\Domain\Action;
 
 use CultuurNet\UDB3\JwtProvider\Domain\Repository\ClientInformationRepositoryInterface;
 use CultuurNet\UDB3\JwtProvider\Domain\Service\GenerateAuthorizedDestinationUrl;
-use CultuurNet\UDB3\JwtProvider\Domain\Repository\DestinationUrlRepositoryInterface;
 use CultuurNet\UDB3\JwtProvider\Domain\Service\LoginServiceInterface;
 use CultuurNet\UDB3\JwtProvider\Domain\Value\ClientInformation;
 use CultuurNet\UDB3\JwtProvider\Infrastructure\Factory\SlimResponseFactory;
@@ -19,11 +18,6 @@ class AuthorizeTest extends TestCase
      */
     public function it_returns_response_with_authorized_url_for_successful_authorization(): void
     {
-        $destinationUrlRepository = $this->prophesize(DestinationUrlRepositoryInterface::class);
-        $destinationUrl = (new UriFactory())->createUri('http://foo-bar.com/');
-
-        $destinationUrlRepository->getDestinationUrl()->willReturn($destinationUrl);
-
         $authService = $this->prophesize(LoginServiceInterface::class);
         $authService->token()->willReturn('token');
         $authService->refreshToken()->willReturn('refresh');
