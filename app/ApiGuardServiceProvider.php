d1e622c82575dc81a2bad5942af4edd256fa68b8
zvonimir <zspajich@gmail.com>
2020-01-29 08:53:57 +0100

    Remove unused service definitions

--- app/ApiGuardServiceProvider.php
+++ app/ApiGuardServiceProvider.php
@@ -2,19 +2,11 @@
 
 namespace CultuurNet\UDB3\JwtProvider;
 
-use CultureFeed;
 use CultuurNet\Auth\ConsumerCredentials;
-use CultuurNet\UDB3\ApiGuard\ApiKey\ApiKeyAuthenticatorInterface;
 use CultuurNet\UDB3\ApiGuard\ApiKey\Reader\ApiKeyReaderInterface;
 use CultuurNet\UDB3\ApiGuard\ApiKey\Reader\CompositeApiKeyReader;
 use CultuurNet\UDB3\ApiGuard\ApiKey\Reader\CustomHeaderApiKeyReader;
 use CultuurNet\UDB3\ApiGuard\ApiKey\Reader\QueryParameterApiKeyReader;
-use CultuurNet\UDB3\ApiGuard\Consumer\ConsumerReadRepositoryInterface;
-use CultuurNet\UDB3\ApiGuard\Consumer\ConsumerWriteRepositoryInterface;
-use CultuurNet\UDB3\ApiGuard\Consumer\InMemoryConsumerRepository;
-use CultuurNet\UDB3\ApiGuard\CultureFeed\CultureFeedApiKeyAuthenticator;
-use CultuurNet\UDB3\ApiGuard\Request\ApiKeyRequestAuthenticator;
-use CultuurNet\UDB3\ApiGuard\Request\RequestAuthenticatorInterface;
 use CultuurNet\UDB3\JwtProvider\Infrastructure\Service\CultureFeedDecorator;
 use ICultureFeed;
 
@@ -22,11 +14,6 @@ class ApiGuardServiceProvider extends BaseServiceProvider
 {
     protected $provides = [
         ApiKeyReaderInterface::class,
-        ApiKeyAuthenticatorInterface::class,
-        RequestAuthenticatorInterface::class,
-        InMemoryConsumerRepository::class,
-        ConsumerWriteRepositoryInterface::class,
-        ConsumerReadRepositoryInterface::class,
         ICultureFeed::class,
     ];
 
@@ -64,58 +51,6 @@ class ApiGuardServiceProvider extends BaseServiceProvider
 
                 $oauthClient->setEndpoint($this->parameter('uitid.base_url'));
                 return new CultureFeedDecorator($oauthClient);
-            }
-        );
-        $this->add(
-            ApiKeyAuthenticatorInterface::class,
-            function () {
-                $consumerCredentials = new ConsumerCredentials(
-                    $this->parameter('uitid.consumer.key'),
-                    $this->parameter('uitid.consumer.secret')
-                );
-
-                $oauthClient = new \CultureFeed_DefaultOAuthClient(
-                    $consumerCredentials->getKey(),
-                    $consumerCredentials->getSecret()
-                );
-                $oauthClient->setEndpoint($this->parameter('uitid.base_url'));
-
-                return new CultureFeedApiKeyAuthenticator(
-                    new CultureFeed($oauthClient),
-                    $this->get(ConsumerSession::class),
-                    true
-                );
-            }
-        );
-
-        $this->add(
-            RequestAuthenticatorInterface::class,
-            function () {
-                return new ApiKeyRequestAuthenticator(
-                    $this->get(ApiKeyReaderInterface::class),
-                    $this->get(ApiKeyAuthenticatorInterface::class)
-                );
-            }
-        );
-
-        $this->addShared(
-            ConsumerReadRepositoryInterface::class,
-            function () {
-                return $this->get(ConsumerSession::class);
-            }
-        );
-
-        $this->addShared(
-            ConsumerWriteRepositoryInterface::class,
-            function () {
-                return $this->get(ConsumerSession::class);
-            }
-        );
-
-        $this->addShared(
-            InMemoryConsumerRepository::class,
-            function () {
-                return new InMemoryConsumerRepository();
             }
         );
     }
