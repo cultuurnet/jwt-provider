b7af9c51445f70225da3ddfe1e4fc27012bd15b5
Luc Wollants <lucwollants@gmail.com>
2020-10-16 11:19:56 +0200

    III-3531 Prepend the Sentry exception handler class

--- web/index.php
+++ web/index.php
@@ -4,16 +4,18 @@ use CultuurNet\UDB3\JwtProvider\Factory\ConfigFactory;
 use CultuurNet\UDB3\JwtProvider\Factory\ContainerFactory;
 use CultuurNet\UDB3\JwtProvider\Factory\ErrorHandlerFactory;
 use League\Route\Router;
+use Sentry\State\HubInterface;
 use Slim\Psr7\Factory\ServerRequestFactory;
 use Zend\HttpHandlerRunner\Emitter\SapiStreamEmitter;
 
-require_once __DIR__ . '/../vendor/autoload.php';
 
-$whoops = ErrorHandlerFactory::create();
-$whoops->register();
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $config = ConfigFactory::create(__DIR__ . '/../');
 $container = ContainerFactory::forWeb($config);
+
+$whoops = ErrorHandlerFactory::create($container->get(HubInterface::class));
+$whoops->register();
 
 $response = $container->get(Router::class)->dispatch(
     ServerRequestFactory::createFromGlobals()
