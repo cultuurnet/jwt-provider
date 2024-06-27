aa49e528e62f7da63b16364d3bd74281623cf9cd
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2024-06-27 10:06:37 +0200

    linting

--- web/jwt-example.php
+++ web/jwt-example.php
@@ -31,7 +31,6 @@ if (!empty($_GET)) {
     echo '<table border="1">';
     echo '<tr><th>Parameter</th><th>Value</th></tr>';
     foreach ($_GET as $key => $value) {
-
         echo '<tr><td>' . htmlspecialchars($key) . '</td><td>' . htmlspecialchars($value) . '</td></tr>';
     }
     echo '</table>';
